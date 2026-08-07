542f7c75296bbec96280234de0d681ec6839c143
iaemeath <iaemeath@163.com>
2026-08-07 15:23:54 +0800

    chore: 清理无用文件（HelloWorld脚手架残留、遗留shell脚本、dev-dist构建产物）

--- check.js
+++
@@ -1,8 +0,0 @@
-const webllm = require('@mlc-ai/web-llm');
-console.log('--- ALL KEYS ---');
-console.log(Object.keys(webllm));
-
-console.log('--- MODELS ---');
-if (webllm.prebuiltAppConfig) {
-  console.log(webllm.prebuiltAppConfig.model_list.map(m => m.model_id).slice(0, 10));
-}
--- setup_pwa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-cd /home/iaemeath/code/todo
-npm install vite-plugin-pwa -D --registry=https://registry.npmmirror.com
-mkdir -p public
-HOST_IP=$(grep nameserver /etc/resolv.conf | awk '{print $2}')
-curl -x http://$HOST_IP:7892 -o public/pwa-192x192.png 'https://ui-avatars.com/api/?name=AI+Todo&size=192&background=random'
-curl -x http://$HOST_IP:7892 -o public/pwa-512x512.png 'https://ui-avatars.com/api/?name=AI+Todo&size=512&background=random'
